53e2cf10e1045e82f66c448d2eafa58ce62a5c05
Odirax <dario.lucic@hotmail.de>
2023-05-12 12:36:50 +0200

    startApp

--- script_01.js
+++ script_01.js
@@ -6,10 +6,57 @@
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
 1. Dateneingabe + -überprüfung : 
-2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+2. Auswahl Rechenart : check!
+3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
+
+// Konstanten
+const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
+const ERROR_STR_GEN = "Irgendetwas ging schief!"
+
+startApp()
+function startApp(){
+	output(calculator(getNum(),getNum(),getOp()));
+}
+
+function getNum() {
+	return parseFloat(prompt("Geben Sie eine Zahl ein:"));
+}
+
+function getOp() {
+	return prompt("Wählen Sie eine Rechenart (+, -, *, :, /) ein:") ;
+}
+
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+// output(calculator(3,2,"+"));
+// output(calculator(3,2,"-"));
+// output(calculator(3,2,"*"));
+// output(calculator(3,2,":"));
+// output(calculator(3,2,"/"));
+// output(calculator(3,0,"/"));
+// output(calculator(3,2,"#?!"));
+
+
+function calculator(a,b,op) {
+
+	switch (op) {
+		case "+":
+			return add(a,b);
+		case "-":
+			return subtract(a,b);
+		case "*":
+			return multiply(a,b);
+		case "/":
+		case ":":
+			return divide(a,b);
+		default:
+			return ERROR_STR_GEN;
+	}
+	
+	// return
+}
 
 // module: divide a/b | test
 
@@ -19,11 +66,10 @@
 // output(divide(3,0));
 function divide(a,b) {
 
-	if (b != 0) {
-		return a/b;
-	} else {
-		return "Teilen durch 0 nicht möglich"
-	}
+	if (b == 0) { // Ausnahme + Abbruch
+		return ERROR_STR_DIV;	
+	}  
+	return a/b; // Regel
 }
 
 // module: multiply a*b | test
