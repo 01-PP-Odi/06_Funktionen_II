0538d04f2efb7ae8e877d0af8b39b4d6f16f0798
Odirax <dario.lucic@hotmail.de>
2023-05-12 10:14:48 +0200

    module: addition a + b

--- script_01.js
+++ script_01.js
@@ -15,6 +15,20 @@
 
 
 
+
+
+
+// module: addition a + b | test
+
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+function add(a,b) {
+	let c = a + b;  
+	return c;   
+}
+
+
 // module: output | test:
 // output("hello");
 // output(2);
